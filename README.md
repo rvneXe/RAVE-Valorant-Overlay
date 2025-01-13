5a38c475dc7386aa852d4fbd8b64dba5c7d56cb4
Ali Nazer <roboatash2015@gmail.com>
2025-01-13 21:20:55 +0330

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# RAVE-Valorant-HUD
-A manually-controlled Observer HUD for Valorant tournaments, intended to be like VCT one.
+# RAVE - A VALORANT Overlay
+
+A manually-controlled Observer HUD to be used for Valorant streaming and observing.
+
+Currently, due to VALORANT Client's limitations, there's no automatic game feeds; Everything on this Tool is manually controlled. It can display **Team Name**, **Logo**, and **map score** for now.
+
+This tool uses **React js**. Everything is controlled through 2 HTML pages for **Administration** and a **Match setup**. The Overlay itself is accessible as another HTML page, which can be used in OBS Browser Source, for example.
+
+# Getting Started
+
+*   **You have to install [Node.js]([https://nodejs.org/en/) in order to run this Overlay.**
+    
+
+1.  Clone the repo
+    
+2.  Open a terminal in repo's folder and run this command:
+    
+
+```
+npm install
+```
+
+3.  Wait until you see this message: `added x packages, and audited x packages`
+    
+
+# How to use
+
+### Starting the overlay
+
+1.  Open a terminal in repo's folder and run this command:
+    
+
+```
+node server.js
+```
+
+2.  The overlay server is started now. Use these addresses to access Admin, Setup, and the Overlay itself:
+    
+
+```
+Admin: localhost:7777/admin.html
+Match setup: localhost:7777/setup.html
+
+The Overlay itself: localhost:7777/overlay.html
+```
+
+### Adding team logos
+
+*   The logo should be a 1:1 Image file, it's recommended to be larger than 200x200px
+    
+
+1.  Copy the logo file in public/userdata/
+    
+2.  Configure it for the desired team in the **Match Setup** page.
+    
+
+# Terms of usage
+
+This tool is available for everyone under GPL-3.0 License. _(TLDR: u can copy, modify and distribute it but under the same license, use privately or for commercial purposes, and it's provided without warranty. [learn more](https://gist.github.com/kn9ts/cbe95340d29fc1aaeaa5dd5c059d2e60))_
+
+### It's not necessary to credit me anywhere if you're using it, but it will be an endless kindness if you do :3333
+
+# Fonts License
+
+I did include some fonts (like `Tungsten`) with this repository, to use in this project and it's purpose only. Any other usage is not intended by me, and will not be my fault, and I'll not be responsible for any illegal usage of those fonts.
---
+++ server.js
@@ -0,0 +1,151 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Path to the state.json file
+const stateFilePath = path.join(__dirname, 'state.json');
+
+// Load the state from the JSON file
+let overlayState = {
+    "format": "bo5",
+    "half": "1",
+    "map": "4",
+    "mapname": "Lotus",
+    "event_name": "Valorant Champions 2024",
+    "event_stage": "Playoffs: Lower Final",
+    "color": "#b6a56d",
+    "timeout": {
+      "isActive": "0",
+      "team": "L",
+      "side": "CT",
+      "L_remain": "2",
+      "R_remain": "2"
+    },
+    "nameL": "LEVIATÁN",
+    "abbrL": "LEV",
+    "logoL": "./userdata/lev.png",
+    "scoreL": 1,
+    "p1L": "Mazino",
+    "p2L": "kiNgg",
+    "p3L": "aspas",
+    "p4L": "tex",
+    "p5L": "C0M",
+    "CL": "Itopata",
+    "nameR": "Team Heretics",
+    "abbrR": "TH",
+    "logoR": "./userdata/th.png",
+    "scoreR": 2,
+    "p1R": "MiniBoo",
+    "p2R": "Boo",
+    "p3R": "RieNs",
+    "p4R": "benjyfishy",
+    "p5R": "Wo0t",
+    "CR": "neilzinho"
+};
+let winby = {"bo5": "3", "bo3": "2", "bo1": "1"};
+
+// Load the state from the JSON file
+function loadStateFile() {
+    if (fs.existsSync(stateFilePath)) {
+        try {
+            const data = fs.readFileSync(stateFilePath, "utf-8");
+            overlayState = JSON.parse(data);
+            console.log("State configuration loaded.");
+        } catch (error) {
+            console.error("Error reading state file:", error);
+        }
+    }
+}
+loadStateFile();
+
+// Save the state to the JSON file
+function saveStateToFile() {
+    try {
+        fs.writeFileSync(stateFilePath, JSON.stringify(overlayState, null, 2));
+        console.log('State configuration saved.');
+    } catch (error) {
+        console.error('Error saving state to file:', error);
+    }
+}
+
+app.use(express.static('public'));
+console.log('====================================');
+console.log('  RAVE VALORANT OBSERVER OVERLAY');
+console.log('  BY RVNeXe.github.io');
+console.log('====================================');
+
+if (!fs.existsSync(stateFilePath)) {
+    fs.writeFileSync(stateFilePath, JSON.stringify(overlayState, null, 2));
+    console.log('!!!! Couldn\'t find State configuration file, So recreated it with default sample state.');
+}
+
+io.on('connection', (socket) => {
+    // console.log('A user connected:', socket.id);
+
+    // Send the current state to the newly connected client
+    socket.emit('initialize-overlay', overlayState);
+
+    // Receive admin commands
+    socket.on('admin-command', (data) => {
+        console.log('Received command:', data);
+
+        // Update the state on the server
+        switch (data.action) {
+            case 'change-color':
+                overlayState.color = data.value;
+                break;
+            case 'update':
+                overlayState[data.target] = data.value;
+                break;
+            case 'reload-file':
+                loadStateFile();
+                break;
+            case 'apply-format':
+                overlayState.format = data.value;
+                overlayState.scoreL = 0;
+                overlayState.scoreR = 0;
+                break;
+            case 'score-add':
+                if (overlayState['score'+data.value] != winby[overlayState.format]) {
+                    overlayState['score'+data.value] ++;
+                }
+                break;
+            case 'score-rem':
+                if (overlayState['score'+data.value] != 0) {
+                    overlayState['score'+data.value] --;
+                }
+                break;
+            default:
+                console.log('Unknown command:', data.action);
+                break;
+        }
+
+        // Save the updated state to the JSON file
+        if (data.action != 'reload-file') {
+            saveStateToFile();
+        }
+
+        // Broadcast the update to all connected clients (overlay and admin)
+        socket.broadcast.emit('update-overlay', overlayState);
+        io.emit('admin-update', overlayState); // Notify admins of the updated state
+    });
+
+    socket.on('disconnect', () => {
+        // console.log('A user disconnected:', socket.id);
+    });
+});
+
+const PORT = 7777;
+server.listen(PORT, () => {
+    console.log(`Server is now running on localhost:${PORT}`);
+    console.log(`- Admin page: http://localhost:${PORT}/admin.html`);
+    console.log(`- Match setup page: http://localhost:${PORT}/setup.html`);
+    console.log(' ');
+    console.log(`- Overlay: http://localhost:${PORT}/overlay.html`);
+});
